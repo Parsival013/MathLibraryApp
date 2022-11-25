d6fa41ab6357856e4fa00dae1f3e19cc1157bc06
user25 <user25@user25.ru>
2022-11-25 13:05:45 +0500

    добавлены комментарии

--- MathLibraryP/MathClass.cs
+++ MathLibraryP/MathClass.cs
@@ -8,6 +8,18 @@ namespace MathLibraryP
 {
     public class MathClass
     {
+        /// <summary>
+        /// Нахождение суммы
+        /// </summary>
+        /// <param name="x">
+        /// Первое значение
+        /// </param>
+        /// <param name="y">
+        /// Второе значение
+        /// </param>
+        /// <returns>
+        /// Возвращает результат суммы
+        /// </returns>
         public static int Sum(int x,int y)
         {
             int s = x + y;
