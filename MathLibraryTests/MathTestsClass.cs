0421278cf39793962ce45d6d921496a9ebdb1843
user25 <user25@user25.ru>
2022-11-25 13:14:19 +0500

    создан проект модульного теста и проделан первый тест

---
+++ MathLibraryTests/MathTestsClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibraryP;
+namespace MathLibraryTests
+{
+    [TestClass]
+    public class MathTestsClass
+    {
+        [TestMethod]
+        public void Sum_1And1_ReturnSum()
+        {
+            int x = 1;
+            int y = 1;
+            int excepted = 2;
+            //Act
+            int actual =MathClass.Sum(x,y);
+            //Assert
+            Assert.AreEqual(excepted, actual);
+        }
+    }
+}
